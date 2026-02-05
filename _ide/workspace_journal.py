6bc2aa10dbee8da53d6b72ebdc9db79d63f4b584
wewanchu <wewanchu@e5-001-w16.ece.ualberta.ca>
2026-02-05 14:38:41 -0700

    asdf

--- _ide/workspace_journal.py
+++ _ide/workspace_journal.py
@@ -6,3 +6,8 @@ client.set_workspace(path="ECE315lab1")
 
 platform = client.create_platform_component(name = "lab1",hw_design = "$COMPONENT_LOCATION/../lab1_hw/ece315_lab1/lab1_hw_wrapper.xsa",os = "freertos",cpu = "ps7_cortexa9_0",domain_name = "freertos_ps7_cortexa9_0")
 
+comp = client.create_app_component(name="app_component",platform = "$COMPONENT_LOCATION/../lab1/export/lab1/lab1.xpfm",domain = "freertos_ps7_cortexa9_0")
+
+comp = client.get_component(name="app_component")
+status = comp.import_files(from_loc="", files=["C:\Users\wewanchu\School\ECE315\lab1\ECE315lab1\part1\lab1_part1.c"])
+
